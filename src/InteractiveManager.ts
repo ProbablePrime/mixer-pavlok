fe1a6eb164ce4f2ed3a23cd295133380b677fb88
ProbablePrime <richard.fox@ahref.co.uk>
2017-07-25 00:31:35 +0200

    D:

---
+++ src/InteractiveManager.ts
@@ -0,0 +1,51 @@
+import { GameClient, IButton, IControl, setWebSocket } from 'beam-interactive-node2';
+import { EventEmitter } from 'events';
+import * as WebSocket from 'ws';
+
+setWebSocket(WebSocket);
+
+export class InteractiveManager extends EventEmitter {
+    private interactive = new GameClient();
+    constructor(private token: string, private id: number) {
+        super();
+    }
+
+    public init(): Promise<void> {
+        return this.interactive.open({
+            versionId: this.id,
+            authToken: this.token,
+        })
+        .then(() => this.interactive.synchronizeState())
+        .then(() => this.createControls())
+        .then(controls => this.setupEvents(controls))
+        .then(() => { /* */});
+    }
+
+    private createControls(): Promise<IControl[]> {
+        const controls = require('../config/controls.json'); //tslint:disable-line: no-require-imports
+        return this.interactive.state.getScene('default').createControls(controls);
+    }
+
+    private setupEvents(controls: IControl[]) {
+        controls.forEach((control: IButton) => {
+            control.on('mousedown', (inputEvent, participant) => {
+                if (inputEvent.transactionID) {
+                    this.interactive.captureTransaction(inputEvent.transactionID)
+                    .then(() => {
+                        if (!control.meta) {
+                            return;
+                        }
+                        const action = control.meta.action.value;
+                        const strength = Number(control.meta.strength.value);
+                        if (!action || !strength) {
+                            return;
+                        }
+                        this.emit(<string> control.meta.action.value, control.meta.strength.value);
+                        console.log(`Charged ${participant.username} ${control.cost} sparks!`);
+                        control.setCooldown(strength * 5000);
+                    });
+                }
+            });
+        });
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import * as config from 'config';
 import * as pavlok from 'pavlok-beta-api-login';
+import { InteractiveManager } from './InteractiveManager';
 
 const clientId = config.get<string>('pavlok.clientID');
 const secretId = config.get<string>('pavlok.clientSecret');
+
+const token = config.get<string>('mixer.token');
+const version = config.get<number>('mixer.version');
+
+
 
 pavlok.init(
     clientId,
@@ -16,6 +22,10 @@ pavlok.login((result: any, code: any) => {
     if (!result) {
         throw new Error('Auth failed');
     }
+    const manager = new InteractiveManager(token, version);
+    manager.init().then(() => {
+        registerEvents();
+    })
 });
 
 
